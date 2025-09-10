0a233542603003975e32be81613dba0e17c063c0
petraalpigiano <petraalpigiano@gmail.com>
2025-09-10 16:51:45 +0200

    add 3 entity

---
+++ database/migrations/2025_09_10_145027_create_eps_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('eps', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', length: 50);
+            $table->year('published_year');
+            $table->tinyInteger('n_songs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('eps');
+    }
+};
