127d05febaaa2f7aec6d1407f7923b7fb7ca7f3d
petraalpigiano <petraalpigiano@gmail.com>
2025-09-11 17:08:11 +0200

    add CRUD for Ep

--- app/Http/Controllers/Admin/EpController.php
+++ app/Http/Controllers/Admin/EpController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Ep;
 use Illuminate\Http\Request;
 
 class EpController extends Controller
@@ -12,7 +13,9 @@ class EpController extends Controller
      */
     public function index()
     {
-        //
+        $eps = Ep::all();
+        dd($eps);
+        return view('prova.index', compact('eps'));
     }
 
     /**
@@ -20,7 +23,7 @@ class EpController extends Controller
      */
     public function create()
     {
-        //
+        return view('prova.create');
     }
 
     /**
@@ -28,38 +31,63 @@ class EpController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->all();
+        dd($data);
+
+        $newEp = new Ep();
+
+        $newEp->name = $data['name'];
+        $newEp->published_year = $data['published_year'];
+        $newEp->n_songs = $data['n_songs'];
+
+        $newEp->save();
+
+        return redirect()->route('prova.show', $newEp->id);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Ep $ep)
     {
-        //
+        // dd($ep);
+        return view('prova.show', compact('ep'));
     }
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Ep $ep)
     {
-        //
+
+        // dd($ep);
+        return view('prova.create', compact('ep'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Ep $ep)
     {
-        //
+        $data = $request->all();
+        dd($data);
+
+        $ep->name = $data['name'];
+        $ep->published_year = $data['published_year'];
+        $ep->n_songs = $data['n_songs'];
+
+        $ep->update();
+
+        return redirect()->route('prova.show', $ep->id);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Ep $ep)
     {
-        //
+        $ep->delete();
+
+        return redirect()->route('prova.index');
     }
 }
